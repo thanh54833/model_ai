0df7626b704c316ee88508de6b81099f07c47911
phamthanh <phamhoaithanh32@gmail.com>
2025-03-22 14:03:28 +0700

    update

--- locustfile.py
+++ locustfile.py
@@ -112,42 +112,13 @@ class LoadTestUser(HttpUser):
     @task
     def search(self):
         keyword = random.choice(self.keywords)["name"]
-        # self.client.post(
-        #     url="/multi_search?x-typesense-api-key=09EqyvhZTK4bRlTGe9WrYNBIi5gUuosl",
-        #     headers={
-        #         "Accept": "application/json",
-        #         "Content-Type": "application/json"
-        #     },
-        #     json={
-        #         "searches": [
-        #             {
-        #                 "collection": "NET6_product_blue",
-        #                 "filter_by": "",
-        #                 "q": keyword,
-        #                 "query_by": "ecom_product_name,com_manufacturer_name,com_category_name,keywords,description",
-        #                 "query_by_weights": "10,10,10,2,1",
-        #                 "sort_by": "_text_match:desc,popularity:desc",
-        #                 "text_match_type": "sum_score",
-        #                 "stopwords": "stopword_set1",
-        #                 "page": 1,
-        #                 "per_page": 36,
-        #                 "facet_by": "*",
-        #                 "max_facet_values": 10,
-        #                 "include_fields": "",
-        #                 "exclude_fields": "is_deleted,erp_product_name,erp_manufacturer_name,erp_manufacturer_id ,embedding_reason_of_failure,created_date,auto_2_embedding,text_embedding,img_embedding,description,promotion_name_unaccent,com_manufacturer_name_unaccent,ecom_product_name_unaccent,com_category_name_unaccent,highlights,product_attributes,image_embedding,keywords,product_image_trans,checksum,embeddingerp_product_id,erp_category_name,erp_category_id, com_manufacturer_image_square_url",
-        #                 "exhaustive_search": False,
-        #                 "num_typos": 0,
-        #                 "prioritize_token_position": False
-        #             }
-        #         ]
-        #     }
-        # )
 
         self.client.post(
-            url="https://ecommerce-uat.concung.vn/api/se/product-search/search_text",
+            url="/api/se/product-search/search_text",
             headers={
                 "Accept": "application/json",
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Il9HSDZrYllkZ3RxZDdLNHNrUW1uSGI4dUh6OCIsImtpZCI6Il9HSDZrYllkZ3RxZDdLNHNrUW1uSGI4dUh6OCJ9.eyJpc3MiOiJjb25jdW5nLmNvbSIsImF1ZCI6ImNvbmN1bmcuY29tL3Jlc291cmNlcyIsImV4cCI6MTc0MjYyOTk1NCwibmJmIjoxNzQyNjI2MzU0LCJjbGllbnRfaWQiOiJDQ0kuU3dhZ2dlci5DbGllbnRDcmVkZW50aWFsIiwic2NvcGUiOiJDQ0kuU3dhZ2dlciIsInN1YiI6IjE3NDIiLCJhdXRoX3RpbWUiOjE3NDI2MDM0NDUsImlkcCI6Imlkc3J2IiwiL1VzZXJJRCI6IjE3NDIiLCIvVXNlcm5hbWUiOiIyNzQxIiwiL0F2YXRhciI6ImF2YS8yMDI0MDkwOS9jZmRiMDVjZi0xMGFjLTQzMmMtODViMC1jNTIxYmI0MzgxMTQucG5nIiwiL1VzZXJUeXBlSUQiOiIxIiwiL1N1cHBsaWVySUQiOiIwIiwiL0Z1bGxuYW1lIjoiTMOqIEjhu5MgSGnhur91IEdpYW5nIiwiL0RlcGFydG1lbnRJRCI6IjI0NyIsIi9Qb3NpdGlvbklEIjoiMTI3IiwiL1N0b3JlSUQiOiI0NDEiLCIvR2VuZGVyIjoiMiIsIi9FbXBsb3llZUlEIjoiMTc0MSIsIi9WUE5Vc2VybmFtZSI6IjIwOTciLCIvVlBOUGFzc3dvcmQiOiJLdWFqWEM5eWJ6IiwiL1JvbGVJRHMiOiJbMzE1MywzMTk0LDMxNzYsMzI4MSwzMjIxLDMzMDksMzMwMSwzMzI4LDM0MDEsMzQyMywzNDQxLDM0NTEsMzQxNCwzMTc1LDMyNDUsMzQ4NywzMTQ3XSIsImFtciI6WyJwYXNzd29yZCJdfQ.X0kVpqreImlyYydvvoQB1ViNrF8jMVhgZ4AiozXLfIpc9XslU3UKSMYrvcylYMw8-j3InH13PF2ow7sh9IgNqJIFEMu8wXUS8F_NGa0wiloi_AUmuECD-gLXkT3mJn6EYI496A2OnEjMPWCC4Rv0NIRS9hkcFDhJCvNQ8uIQHO9xS7V1N3_If6JUTTHOhVPzJXfPhwGm-YD5a-aeJvqL4Qf_NwYVUbGoYXoxXKxR0Awujux7HrTB11OsaNchD8WmGDY-XhFQwi3Vq1ozIkURD0rOESqaiTIXbAQ7ZMJBes0QO8OWOo8JeMIP-Q9U8xV8RKmmTDdQXjKPFND9wg5_wg"
             },
             json={
                 "q": keyword,
